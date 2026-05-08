d41f317a204440b836eba2a3668443aef21558d5
tallerjugandoaprendoind-glitch <tallerjugandoaprendoind@gmail.com>
2026-05-08 18:10:58 -0500

    ijdsijsdij

--- app/(student)/learn/[id]/CoursePreviewPage.tsx
+++ app/(student)/learn/[id]/CoursePreviewPage.tsx
@@ -78,6 +78,7 @@ export default function CoursePreviewPage(props: Props) {
               userEmail={userEmail}
               waUrl={waUrl}
             />
+          </div>
         </div>
       </div>
 
